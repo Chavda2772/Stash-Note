237bf8e0dd33398fc219e51e5bc251f84999e968
Chavda Mahesh <chavdamahesh2772@gmail.com>
2024-05-02 09:31:57 +0530

    change request limit to 10mb

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV != 'production') {
   require('dotenv').config();
 }
 
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
